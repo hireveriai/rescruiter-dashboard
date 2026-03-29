1a699445653208c44ce8d3a818c137b5f86944af
Jatin Singh <jatin.singh@verihireai.work>
2026-03-29 10:57:04 +0530

    Recruiter Page v7

---
+++ types/pg.d.ts
@@ -0,0 +1,7 @@
+﻿declare module "pg" {
+  export class Pool {
+    constructor(config?: unknown)
+    query<T = unknown>(queryText: string, values?: unknown[]): Promise<{ rows: T[] }>
+    end(): Promise<void>
+  }
+}
